ea14b359d791b81a4b11d61459eff310cbde66b3
Seif Kobrosly <seifkobrosly@gmail.com>
2018-07-29 16:37:48 -0400

    Initial Commit.

---
+++ DC Metro/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DC Metro
+//
+//  Created by Seif Kobrosly on 7/29/18.
+//  Copyright © 2018 District Meta Works, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
